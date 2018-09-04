48385c9361a64de7562c54b6f388e819112b6597
Anton Hlinisty <anton.hlinisty@gmail.com>
2018-09-05 00:24:20 +0300

    #5. Create MessageController with ping method available

--- src/main/java/club/tempvs/message/MessageController.java
+++ src/main/java/club/tempvs/message/MessageController.java
@@ -1,16 +1,11 @@
 package club.tempvs.message;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class MessageController {
-
-    @Autowired
-    CommandLineRunner commandLineRunner;
 
     @RequestMapping("/ping")
     public String getPong() {
--- src/test/java/club/tempvs/message/MessageControllerTest.java
+++ src/test/java/club/tempvs/message/MessageControllerTest.java
@@ -1,32 +1,20 @@
 package club.tempvs.message;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.http.MediaType.*;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
 public class MessageControllerTest {
 
-    @Autowired
-    private MockMvc mvc;
+    private MessageController messageController;
+
+    @Before
+    public void setup() {
+        messageController = new MessageController();
+    }
 
     @Test
-    public void testGetPong() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/api/ping").accept(TEXT_PLAIN))
-                .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("pong!")));
+    public void testGetPong() {
+        assertEquals("getPong() method returns 'pong!' string", "pong!", messageController.getPong());
     }
 }
