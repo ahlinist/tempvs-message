45ba4c32eaaed6cfa83899130613312dc49c4446
Anton Hlinisty <anton.hlinisty@gmail.com>
2018-09-08 01:30:50 +0300

    #5. Update ObjectFactory with constructor handling

--- src/main/java/club/tempvs/message/util/ObjectFactory.java
+++ src/main/java/club/tempvs/message/util/ObjectFactory.java
@@ -2,4 +2,5 @@ package club.tempvs.message.util;
 
 public interface ObjectFactory {
     <T> T getInstance(Class<T> clazz);
+    <T> T getInstance(Class<T> clazz, Object... args);
 }
--- src/main/java/club/tempvs/message/util/ObjectFactoryImpl.java
+++ src/main/java/club/tempvs/message/util/ObjectFactoryImpl.java
@@ -2,6 +2,8 @@ package club.tempvs.message.util;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @Component
 public class ObjectFactoryImpl implements ObjectFactory {
 
@@ -11,6 +13,21 @@ public class ObjectFactoryImpl implements ObjectFactory {
 
         try {
             instance = clazz.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return instance;
+    }
+
+    @Override
+    public <T> T getInstance(Class<T> clazz, Object... args) {
+        T instance = null;
+
+        Class[] parameterTypes = Arrays.stream(args).map(x -> x.getClass()).toArray(Class[]::new);
+
+        try {
+            instance = clazz.getConstructor(parameterTypes).newInstance(args);
         } catch (Exception e) {
             e.printStackTrace();
         }
