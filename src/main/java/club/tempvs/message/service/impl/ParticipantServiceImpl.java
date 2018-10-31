a81f72b3fce1813928d72c6f5b417ab6d8e74dd6
Anton Hlinisty <anton.hlinisty@gmail.com>
2018-11-01 00:55:16 +0300

    #32. Remove unused imports

--- src/main/java/club/tempvs/message/service/impl/ParticipantServiceImpl.java
+++ src/main/java/club/tempvs/message/service/impl/ParticipantServiceImpl.java
@@ -6,7 +6,6 @@ import club.tempvs.message.service.ParticipantService;
 import club.tempvs.message.util.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ParticipantServiceImpl implements ParticipantService {
